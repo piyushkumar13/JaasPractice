7b62e16f038cfaf86b1809184dbe48302e21610f
PIYUSH KUMAR <piyush.k9013@gmail.com>
2016-06-26 20:51:41 +0530

    Checking in test class

---
+++ src/test/java/com/practice/jaas/BasicAuthenticatedServletTest.java
@@ -0,0 +1,14 @@
+package com.practice.jaas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicAuthenticatedServletTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
