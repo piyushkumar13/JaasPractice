3cc268e2eaa1651aa51f2df0463167804a06abe0
PIYUSH KUMAR <piyush.k9013@gmail.com>
2016-07-03 18:13:51 +0530

    Jaas Programmatic approach

---
+++ src/main/java/com/practice/jaas/programmatic/HttpServletImpl.java
@@ -0,0 +1,13 @@
+package com.practice.jaas.programmatic;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HttpServletImpl extends HttpServlet {
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+
+}
