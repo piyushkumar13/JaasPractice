6acaad681cb56a8bb1790a5316badbc34fad4827
PIYUSH KUMAR <piyush.k9013@gmail.com>
2016-07-06 21:35:57 +0530

    Annotation based security

---
+++ src/main/java/com/practice/jaas/annotation/JaasAnnotationExample.java
@@ -0,0 +1,35 @@
+package com.practice.jaas.annotation;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.HttpConstraint;
+import javax.servlet.annotation.HttpMethodConstraint;
+import javax.servlet.annotation.ServletSecurity;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/auth/annotation")
+@ServletSecurity(httpMethodConstraints = { @HttpMethodConstraint(
+		value = "POST",
+		rolesAllowed = { "javaee" }) })
+public class JaasAnnotationExample extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter writer = resp.getWriter();
+		writer.write("Hi! this is the servelet security in get request.");
+
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		PrintWriter writer = resp.getWriter();
+		resp.setContentType("text/html");
+		writer.write("Hi! this is the servelet security in post request.");
+
+	}
+}
